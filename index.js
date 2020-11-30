c82251ee72a3b1ef82fc13c3913a8ca63a7a37d0
Jorge Garcia <jiorgio.garcia@gmail.com>
2020-11-30 17:16:00 -0300

    ejercicios resueltos

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ const person = require('./names.js');
 const fs = require('fs');
 const catMe = require('cat-me');
 
-// console.log(person.person.name)
-
+console.log('Node TKM')
 console.log(`Mi nombre es ${person.person.name} y tengo ${person.person.age} años`);
-
 console.log(`${catMe()} ese plato de comida no se va a llenar solo`);
 
-// fs.writeFile("archivo.txt", person.person.age, () => { 
-//   console.log("archivo creado");
-// });
 
-fs.writeFile("archivo.txt", person.person.name, () => {
+fs.writeFile("nuevo.txt", `Hola soy ${person.person.name} y tengo ${person.person.age} años`, () => {
   console.log("Archivo credo");
 })
